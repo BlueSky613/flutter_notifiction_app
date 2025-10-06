d1beb674b929496773b5d1aa33366599eeab4476
UncertainWorld <159841920+BlueSky613@users.noreply.github.com>
2025-10-06 13:05:04 -0400

    update initstate duration seconds 15

--- lib/widgets/animated_wave_background.dart
+++ lib/widgets/animated_wave_background.dart
@@ -22,7 +22,7 @@ class _AnimatedWaveBackgroundState extends State<AnimatedWaveBackground>
     // This controller will run indefinitely and rebuild the painter every frame
     _waveController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 10),
+      duration: const Duration(seconds: 15),
     )..repeat(); // loop forever
   }
 
