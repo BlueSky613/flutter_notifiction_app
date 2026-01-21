f837df101544aa335c73896ef3b71e54700d1ebd
UncertainWorld <159841920+BlueSky613@users.noreply.github.com>
2026-01-21 00:56:12 -0500

    Increase size of spacer in tasbih_page

--- lib/pages/tasbih_page.dart
+++ lib/pages/tasbih_page.dart
@@ -107,7 +107,7 @@ class _TasbihPageState extends State<TasbihPage> {
                 color: theme.colorScheme.onBackground,
               ),
             ),
-            const SizedBox(height: 40),
+            const SizedBox(height: 45),
             GestureDetector(
               onTap: _increment,
               onLongPress: _isRunning ? _stopAutoIncrement : _startAutoIncrement,
